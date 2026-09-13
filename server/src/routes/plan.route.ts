3d37a7682a5e30ccc11d79b6cc08c4f2777a7773
Muhammad Ahmed <ahmedjutt9253@gmail.com>
2026-09-14 00:42:02 +0500

    table for plan is created and models generated

--- server/src/routes/plan.route.ts
+++ server/src/routes/plan.route.ts
@@ -1,9 +1,25 @@
 import { Router, type Request, type Response } from "express";
+import { prisma } from "../lib/prisma";
 
 export const planRouter = Router();
 
 planRouter.post("/", async (req: Request, res: Response) => {
   try {
+    const { userId } = req.body;
+
+    if (!userId) {
+      res.status(400).json({ error: "User Id Is required" });
+    }
+
+    const profile = await prisma.user_profiles.findUnique({
+      where: { user_id: userId },
+    });
+
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ error: "User profile not found complete Onboarding first" });
+    }
   } catch (error) {
     console.error("Error generating plans ", error);
     res.status(500).json({ error: "Failed to generate plan" });
