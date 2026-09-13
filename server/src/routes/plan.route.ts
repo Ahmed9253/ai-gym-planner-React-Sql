c26a65230762282fbbf6d0ae6858659d43630742
Muhammad Ahmed <ahmedjutt9253@gmail.com>
2026-09-14 00:30:21 +0500

    AI integration left

--- server/src/routes/plan.route.ts
+++ server/src/routes/plan.route.ts
@@ -1,3 +1,11 @@
 import { Router, type Request, type Response } from "express";
 
 export const planRouter = Router();
+
+planRouter.post("/", async (req: Request, res: Response) => {
+  try {
+  } catch (error) {
+    console.error("Error generating plans ", error);
+    res.status(500).json({ error: "Failed to generate plan" });
+  }
+});
