dad4fa81318bf0c9a514e9a8008cdd6e6e6296ce
Muhammad Ahmed <ahmedjutt9253@gmail.com>
2026-09-14 01:02:44 +0500

    ai integration left

--- server/src/routes/plan.route.ts
+++ server/src/routes/plan.route.ts
@@ -20,6 +20,30 @@ planRouter.post("/", async (req: Request, res: Response) => {
         .status(400)
         .json({ error: "User profile not found complete Onboarding first" });
     }
+
+    const latestPlan = await prisma.training_plans.findFirst({
+      where: { user_id: userId },
+      orderBy: { created_at: "desc" },
+      select: { version: true },
+    });
+
+    const nextVersion = latestPlan ? latestPlan.version + 1 : 1;
+
+    let planJson;
+
+    const planText = JSON.stringify(planJson, null, 2);
+    const newPlan = await prisma.training_plans.creat({
+      data: { user_id: userId },
+      plan_json: planJson as any,
+      plan_text: planText,
+      version: nextVersion,
+    });
+
+    return res.json({
+      id: newPlan.id,
+      version: nextVersion,
+      createdAt: newPlan.created_at,
+    });
   } catch (error) {
     console.error("Error generating plans ", error);
     res.status(500).json({ error: "Failed to generate plan" });
